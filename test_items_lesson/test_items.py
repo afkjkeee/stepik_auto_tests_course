5b05d57cf7afccf62fe6e77fd95b8d726e0d7ec6
Artyom B. <artyom.b@cls.global>
2026-01-20 16:20:58 +0300

    fixture lesson fix

--- test_items_lesson/test_items.py
+++ test_items_lesson/test_items.py
@@ -3,6 +3,11 @@ from selenium.webdriver.common.by import By
 def test_add_to_basket_button(browser):
     link = "https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
-    add_to_basket_button = browser.find_element(
+    test_pass = False
+    try:
+        browser.find_element(
                     By.CSS_SELECTOR, "#add_to_basket_form > button")
-    assert add_to_basket_button, "Button 'Add to basket' is missing on the page" 
+        test_pass = True
+    except:
+        pass
+    assert test_pass == True, "Button 'Add to basket' is missing on the page" 
